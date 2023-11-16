437cae0df5557d25db52a463fce0d69051552f87
Pawel Hoc <pawel.hoc@gmail.com>
2023-11-16 22:14:37 +0100

    ortofoto layers

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ import streamlit as st
 
 import streamlit_folium as st_folium
 import folium
-from folium.plugins import MousePosition, Fullscreen
-from streamlit_folium import folium_static
+from folium.plugins import MousePosition, Fullscreen, Draw
 
 
 st.set_page_config(layout = 'wide')
@@ -12,7 +11,8 @@ hide_st_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
-            header {visibility: hidden;}
+            header {visibility: hidden !important;}
+            [data-testid="stToolbar"] {visibility: hidden !important;}
             .css-1rs6os {visibility: hidden;}
             .css-17ziqus {visibility: hidden;}
             .block-container {
@@ -25,65 +25,93 @@ hide_st_style = """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
 
-map_geo = folium.Map(location=[52.0,19.0], zoom_start=6) #.add_child(folium.LatLngPopup())
+map_geo = folium.Map(location=[52.0,19.0], zoom_start=6, control_scale=True, prefer_canvas=True) #.add_child(folium.LatLngPopup())
 
 MousePosition().add_to(map_geo)
 Fullscreen().add_to(map_geo)
+Draw(export=True).add_to(map_geo)
 
 #folium.Figure(width='100%')
 
-folium.raster_layers.WmsTileLayer(url ='https://mapy.geoportal.gov.pl/wss/service/PZGIK/NMT/WMS/SkorowidzeUkladEVRF2007?',
-                layers = 'SkorowidzeNMT2023',
-                transparent = True, 
-                control = True,
-                fmt="image/png",
-                name = 'SkorowidzeNMT2023',
-                overlay = True,
-                show = True,
-                CRS = 'EPSG:2180',
-                version = '1.3.0',
-                opacity=0.35,
-                ).add_to(map_geo)
+# folium.raster_layers.WmsTileLayer(url ='https://mapy.geoportal.gov.pl/wss/service/PZGIK/NMT/WMS/SkorowidzeUkladEVRF2007?',
+#                 layers = 'SkorowidzeNMT2023',
+#                 transparent = True, 
+#                 control = True,
+#                 fmt="image/png",
+#                 name = 'SkorowidzeNMT2023',
+#                 overlay = True,
+#                 show = True,
+#                 CRS = 'EPSG:2180',
+#                 version = '1.3.0',
+#                 opacity=0.35,
+#                 ).add_to(map_geo)
 
-folium.raster_layers.WmsTileLayer(url ='https://mapy.geoportal.gov.pl/wss/service/PZGIK/NMT/WMS/SkorowidzeUkladEVRF2007?',
-                layers = 'SkorowidzeNMT2022',
+# folium.raster_layers.WmsTileLayer(url ='https://mapy.geoportal.gov.pl/wss/service/PZGIK/NMT/WMS/SkorowidzeUkladEVRF2007?',
+#                 layers = 'SkorowidzeNMT2022',
+#                 transparent = True, 
+#                 control = True,
+#                 fmt="image/png",
+#                 name = 'SkorowidzeNMT2022',
+#                 overlay = True,
+#                 show = True,
+#                 CRS = 'EPSG:2180',
+#                 version = '1.3.0',
+#                 opacity=0.35,
+#                 ).add_to(map_geo)
+#
+# folium.raster_layers.WmsTileLayer(url ='https://mapy.geoportal.gov.pl/wss/service/PZGIK/NMT/WMS/SkorowidzeUkladEVRF2007?',
+#                 layers = 'SkorowidzeNMT2021',
+#                 transparent = True, 
+#                 control = True,
+#                 fmt="image/png",
+#                 name = 'SkorowidzeNMT2021',
+#                 overlay = True,
+#                 show = True,
+#                 CRS = 'EPSG:2180',
+#                 version = '1.3.0',
+#                 opacity=0.35,
+#                 ).add_to(map_geo)
+#
+# folium.raster_layers.WmsTileLayer(url ='https://mapy.geoportal.gov.pl/wss/service/PZGIK/NMT/WMS/SkorowidzeUkladEVRF2007?',
+#                 layers = 'SkorowidzeNMT2020iStarsze',
+#                 transparent = True, 
+#                 control = True,
+#                 fmt="image/png",
+#                 name = 'SkorowidzeNMT2020iStarsze',
+#                 overlay = True,
+#                 show = True,
+#                 CRS = 'EPSG:2180',
+#                 version = '1.3.0',
+#                 opacity=0.35,
+#                 ).add_to(map_geo)
+                
+folium.raster_layers.WmsTileLayer(url ='https://mapy.geoportal.gov.pl/wss/service/PZGIK/ORTO/WMS/StandardResolution?',
+                layers = 'Raster',
                 transparent = True, 
                 control = True,
                 fmt="image/png",
-                name = 'SkorowidzeNMT2022',
+                name = 'Ortofotomapa standardowa',
                 overlay = True,
                 show = True,
-                CRS = 'EPSG:2180',
+                #CRS = 'EPSG:2180',
                 version = '1.3.0',
-                opacity=0.35,
+                opacity=0.4,
                 ).add_to(map_geo)
-
-folium.raster_layers.WmsTileLayer(url ='https://mapy.geoportal.gov.pl/wss/service/PZGIK/NMT/WMS/SkorowidzeUkladEVRF2007?',
-                layers = 'SkorowidzeNMT2021',
+                         
+folium.raster_layers.WmsTileLayer(url ='https://mapy.geoportal.gov.pl/wss/service/PZGIK/ORTO/WMS/HighResolution?',
+                layers = 'Raster',
                 transparent = True, 
                 control = True,
                 fmt="image/png",
-                name = 'SkorowidzeNMT2021',
+                name = 'Ortofotomapa o wysokiej rozdzielczości',
                 overlay = True,
-                show = True,
-                CRS = 'EPSG:2180',
+                show = False,
+                #CRS = 'EPSG:2180',
                 version = '1.3.0',
-                opacity=0.35,
+                opacity=0.4,
                 ).add_to(map_geo)
                 
-folium.raster_layers.WmsTileLayer(url ='https://mapy.geoportal.gov.pl/wss/service/PZGIK/NMT/WMS/SkorowidzeUkladEVRF2007?',
-                layers = 'SkorowidzeNMT2020iStarsze',
-                transparent = True, 
-                control = True,
-                fmt="image/png",
-                name = 'SkorowidzeNMT2020iStarsze',
-                overlay = True,
-                show = True,
-                CRS = 'EPSG:2180',
-                version = '1.3.0',
-                opacity=0.35,
-                ).add_to(map_geo)
-                
+                                                  
 folium.LayerControl().add_to(map_geo)
 
 #with st.expander("", expanded=True):
